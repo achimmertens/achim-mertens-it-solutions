7ab42faa6b270ce062d5b716fb1937751ab811ed
achimmertens <achim_mertens@gmx.de>
2026-05-27 12:12:38 +0200

    fix: nächster Versuch die Webseite zu deployen

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,7 +7,6 @@ export default defineConfig({
   base: '/Achim-Mertens-IT-Solutions/',  // Wichtig!
   plugins: [
     tsconfigPaths(),
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
