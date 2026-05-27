254247f43dbc23356286df811c13fd998af4c9e7
achimmertens <achim_mertens@gmx.de>
2026-05-27 12:33:59 +0200

    fix: relative Pfade eingeführt

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/Achim-Mertens-IT-Solutions/',  // Wichtig!
+  base: './',
   plugins: [
     tsconfigPaths(),
     react(),
     tailwindcss(),
   ],
-  server: { host: "::", port: 8080, strictPort: true },
-  preview: { host: "::", port: 8080, strictPort: true },
 });
